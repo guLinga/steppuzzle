277991635be2e945d1b339198aa12eb2d3135a72
GuLing <2634917964@qq.com>
2023-05-17 20:52:45 +0800

    feat: plugin create TOC

---
+++ src/node/plugin-mdx/reMarkPlugins/toc.ts
@@ -0,0 +1,61 @@
+import { Root } from 'mdast';
+import { Plugin } from 'unified';
+import { visit } from 'unist-util-visit';
+import Slugger from 'github-slugger';
+import { parse } from 'acorn';
+import type { MdxjsEsm } from 'mdast-util-mdxjs-esm';
+
+const slugger = new Slugger();
+
+interface TocItem {
+  id: string;
+  text: string;
+  depth: number;
+}
+
+interface ChildNode {
+  type: 'link' | 'text' | 'inlineCode';
+  value: string;
+  children?: ChildNode[];
+}
+
+export const remarkPluginToc: Plugin<[], Root> = () => {
+  return (tree) => {
+    const toc: TocItem[] = [];
+    visit(tree, 'heading', (node) => {
+      if (!node.depth || !node.children?.length) return;
+      // 摘取 h1 ~ h6 的节点
+      if (node.depth > 0 && node.depth < 7) {
+        // debugger;
+        const originText = (node.children as ChildNode[])
+          .map((child) => {
+            switch (child.type) {
+              case 'link':
+                return child.children?.map((c) => c.value).join('') || '';
+              default:
+                return child.value;
+            }
+          })
+          .join('');
+        // 对标题文本进行规范化
+        const id = slugger.slug(originText);
+        toc.push({
+          id,
+          text: originText,
+          depth: node.depth
+        });
+      }
+    });
+    const insertCode = `export const toc = ${JSON.stringify(toc, null, 2)};`;
+    tree.children.push({
+      type: 'mdxjsEsm',
+      value: insertCode,
+      data: {
+        estree: parse(insertCode, {
+          ecmaVersion: 2020,
+          sourceType: 'module'
+        }) as unknown
+      }
+    } as MdxjsEsm);
+  };
+};
