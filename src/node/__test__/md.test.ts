2c09ac0e36db0636be315460abed6ff70f7b72e1
GuLing <2634917964@qq.com>
2023-05-17 14:52:14 +0800

    feat: rehypePlugin

--- src/node/__test__/md.test.ts
+++ src/node/__test__/md.test.ts
@@ -2,11 +2,16 @@ import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkPehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
+import { rehypePluginPreWrapper } from '../plugin-mdx/rehypePlugins/preWrapper';
 import { describe, expect, test } from 'vitest';
 
 describe('Markddown compile cases', async () => {
   const processor = unified();
-  processor.use(remarkParse).use(remarkPehype).use(rehypeStringify);
+  processor
+    .use(remarkParse)
+    .use(remarkPehype)
+    .use(rehypePluginPreWrapper)
+    .use(rehypeStringify);
   test('markdown', async () => {
     const mdContent = '# 123';
     const result = processor.processSync(mdContent);
@@ -18,5 +23,13 @@ describe('Markddown compile cases', async () => {
     expect(result.value).toMatchInlineSnapshot(
       '"<p><code>mermaid graph TB; 1--->2</code></p>"'
     );
+  });
+  test('markdown code block', async () => {
+    const mdContent = '```js\nconsole.log("test")\n```';
+    const result = processor.processSync(mdContent);
+    expect(result.value).toMatchInlineSnapshot(`
+      "<div class=\\"language-js\\"><span class=\\"lang\\">js</span><pre><code class=\\"\\">console.log(\\"test\\")
+      </code></pre></div>"
+    `);
   });
 });
