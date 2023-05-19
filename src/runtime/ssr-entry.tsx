65a255537042529009b8afeed2da524a996aab90
GuLing <2634917964@qq.com>
2023-05-19 12:10:08 +0800

    fix: build no Context

--- src/runtime/ssr-entry.tsx
+++ src/runtime/ssr-entry.tsx
@@ -1,13 +1,17 @@
-import { App } from './App';
+import { App, initPageData } from './App';
 
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
+import { DataContent } from './hooks';
 
-export function render(pagePath: string) {
+export async function render(pagePath: string) {
+  const pageData = await initPageData(pagePath);
   return renderToString(
-    <StaticRouter location={pagePath}>
-      <App />
-    </StaticRouter>
+    <DataContent.Provider value={pageData}>
+      <StaticRouter location={pagePath}>
+        <App />
+      </StaticRouter>
+    </DataContent.Provider>
   );
 }
 
