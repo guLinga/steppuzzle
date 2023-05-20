53c4c1ed8dfe9996b0f4676cfe6c457e2a16cf62
GuLing <2634917964@qq.com>
2023-05-20 14:52:17 +0800

    feat: home features add image

--- src/theme-default/components/HomeFeature/index.tsx
+++ src/theme-default/components/HomeFeature/index.tsx
@@ -1,12 +1,21 @@
 import { Feature } from 'shared/types';
 
 export function HomeFeature(props: { features: Feature[] }) {
+  // console.log('features', props.features);
   return (
     <div className="max-w-1152px" m="auto" flex="~ wrap" justify="between">
       {props.features.map((feature) => {
-        const { icon, title, details } = feature;
+        const { icon, title, details, link, image } = feature;
         return (
-          <div key={title} border="rounded-md" p="r-4 b-4" w="1/3">
+          <a
+            key={title}
+            href={link}
+            border="rounded-md"
+            p="r-4 b-4"
+            w="1/3"
+            target="_blank"
+            rel="noreferrer"
+          >
             <article
               bg="bg-soft"
               border="~ bg-soft solid rounded-xl"
@@ -18,14 +27,14 @@ export function HomeFeature(props: { features: Feature[] }) {
                 border="rounded-md"
                 className="mb-5 w-12 h-12 text-3xl flex-center"
               >
-                {icon}
+                {image ? <img src={image} alt="☑️" /> : icon}
               </div>
               <h2 font="bold">{title}</h2>
               <p text="sm text-2" font="medium" className="pt-2 leading-6">
                 {details}
               </p>
             </article>
-          </div>
+          </a>
         );
       })}
     </div>
