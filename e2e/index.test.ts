7501683a7a97144c3c1bd8dab330201c60dcb0f5
guLinga <2634917964@qq.com>
2023-05-20 18:41:45 +0800

    test: e2etest

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -4,7 +4,7 @@ const siteUrl = 'http://localhost:5173';
 
 test('Verify that the page renders properly', async ({ page }) => {
   await page.goto(siteUrl);
-  // 保证页面已经渲染完毕 测试
+  // 保证页面已经渲染完毕
   await page.waitForLoadState('networkidle');
   const res = await page.evaluate(async () => {
     const pageContent = document.body.innerText;
