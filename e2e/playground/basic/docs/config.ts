24066f2594eaee8a2b3067545604e1528065e6df
guLinga <2634917964@qq.com>
2023-05-22 11:02:26 +0800

    test: config file

---
+++ e2e/playground/basic/docs/config.ts
@@ -0,0 +1,11 @@
+export default ({
+  title: "GuLinga",
+  github: 'https://github.com/GuLinga',
+  githubRepositories: '',
+  themeConfig: {
+    nav: [
+      { text: "主页", link: "/" }
+    ],
+    sidebar: {}
+  },
+})
